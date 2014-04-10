ed5923e4fa9b01863bba21b640a165facaa02417
Florian Pose <fp@igh-essen.com>
2014-04-10 11:20:22 +0200

    About dialog and datasource menu.

--- gui/dlsgui_de.ts
+++ gui/dlsgui_de.ts
@@ -2,6 +2,34 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="de_DE">
 <context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="AboutDialog.ui" line="14"/>
+        <source>About dlsgui</source>
+        <translation>Über dlsgui</translation>
+    </message>
+    <message utf8="true">
+        <location filename="AboutDialog.ui" line="51"/>
+        <source>dlsgui – Graphical User Interface for DLS</source>
+        <translation>dlsgui – Grafische Benutzeroberfläche für den DLS</translation>
+    </message>
+    <message>
+        <location filename="AboutDialog.ui" line="61"/>
+        <source>Open-Source Data Logging Service</source>
+        <translation>Open-Source Messdatenerfassungsystem</translation>
+    </message>
+    <message utf8="true">
+        <location filename="AboutDialog.ui" line="74"/>
+        <source>Copyright © 2014 Florian Pose &lt;fp@igh.de&gt;</source>
+        <translation>Copyright © 2014 Florian Pose &lt;fp@igh.de&gt;</translation>
+    </message>
+    <message>
+        <location filename="AboutDialog.ui" line="89"/>
+        <source>http://etherlab.org/en/dls</source>
+        <translation>http://etherlab.org/en/dls</translation>
+    </message>
+</context>
+<context>
     <name>LogWindow</name>
     <message>
         <location filename="LogWindow.ui" line="14"/>
@@ -43,8 +71,9 @@
     </message>
     <message>
         <location filename="MainWindow.ui" line="80"/>
-        <source>&amp;Recent files</source>
-        <translation>&amp;Zuletzt geöffnet</translation>
+        <source>&amp;Recent views</source>
+        <oldsource>&amp;Recent files</oldsource>
+        <translation>&amp;Zuletzt geöffnete Ansichten</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="97"/>
@@ -62,132 +91,168 @@
         <translation>Scripte</translation>
     </message>
     <message>
-        <location filename="MainWindow.ui" line="123"/>
+        <location filename="MainWindow.ui" line="114"/>
+        <source>About</source>
+        <translation>Über</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="120"/>
+        <source>Datasource</source>
+        <translation>Datenquelle</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="137"/>
         <source>&amp;Close</source>
         <translation>&amp;Schließen</translation>
     </message>
     <message>
-        <location filename="MainWindow.ui" line="126"/>
+        <location filename="MainWindow.ui" line="140"/>
         <source>Close the application</source>
         <translation>Schließt die Applikation</translation>
     </message>
     <message>
-        <location filename="MainWindow.ui" line="135"/>
-        <source>&amp;Open...</source>
-        <translation>&amp;Öffnen...</translation>
+        <location filename="MainWindow.ui" line="149"/>
+        <source>&amp;Open view...</source>
+        <translation>Ansicht &amp;öffnen...</translation>
     </message>
     <message>
-        <location filename="MainWindow.ui" line="138"/>
+        <location filename="MainWindow.ui" line="161"/>
+        <source>&amp;Save view</source>
+        <translation>Ansicht &amp;speichern</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="173"/>
+        <source>Save view &amp;as...</source>
+        <translation>Ansicht speichern &amp;als...</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="198"/>
+        <source>New view</source>
+        <translation>Neue Ansicht</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="203"/>
+        <source>About dlsgui...</source>
+        <translation>Über dlsgui...</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="212"/>
+        <source>&amp;Add Datasource...</source>
+        <translation>&amp;Datenquelle hinzufügen...</translation>
+    </message>
+    <message>
+        <source>&amp;Open...</source>
+        <translation type="obsolete">&amp;Öffnen...</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="152"/>
         <source>Open a stored view</source>
         <translation>Öffnet eine gespeicherte Ansicht</translation>
     </message>
     <message>
-        <location filename="MainWindow.ui" line="147"/>
         <source>&amp;Save</source>
-        <translation>&amp;Speichern</translation>
+        <translation type="obsolete">&amp;Speichern</translation>
     </message>
     <message>
-        <location filename="MainWindow.ui" line="150"/>
+        <location filename="MainWindow.ui" line="164"/>
         <source>Save the current view</source>
         <translation>Speichert die aktiuelle Ansicht</translation>
     </message>
     <message>
-        <location filename="MainWindow.ui" line="159"/>
         <source>Save &amp;as...</source>
-        <translation>Speichern &amp;unter...</translation>
+        <translation type="obsolete">Speichern &amp;unter...</translation>
     </message>
     <message>
-        <location filename="MainWindow.ui" line="162"/>
+        <location filename="MainWindow.ui" line="176"/>
         <source>Save as</source>
         <translation>Speichern unter</translation>
     </message>
     <message>
-        <location filename="MainWindow.ui" line="165"/>
+        <location filename="MainWindow.ui" line="179"/>
         <source>Save the current view to a different file</source>
         <translation>Speichert die aktuelle Ansicht in einer anderen Datei</translation>
     </message>
     <message>
-        <location filename="MainWindow.ui" line="170"/>
+        <location filename="MainWindow.ui" line="184"/>
         <source>Settings</source>
         <translation>Einstellungen</translation>
     </message>
     <message>
-        <location filename="MainWindow.ui" line="175"/>
+        <location filename="MainWindow.ui" line="189"/>
         <source>Log Window</source>
         <translation>Meldungsfenster</translation>
     </message>
     <message>
-        <location filename="MainWindow.ui" line="180"/>
         <source>New</source>
-        <translation>Neu</translation>
+        <translation type="obsolete">Neu</translation>
     </message>
     <message>
-        <location filename="MainWindow.cpp" line="110"/>
+        <location filename="MainWindow.cpp" line="113"/>
         <source>Unknown dialect &quot;%1&quot;: Defaulting to Yaml.</source>
         <translation>Unbekannter Dialekt &quot;%1&quot;: Verwende Yaml.</translation>
     </message>
     <message>
-        <location filename="MainWindow.cpp" line="221"/>
+        <location filename="MainWindow.cpp" line="232"/>
         <source>&amp;%1 %2</source>
         <translation>&amp;%1 %2</translation>
     </message>
     <message>
-        <location filename="MainWindow.cpp" line="250"/>
+        <location filename="MainWindow.cpp" line="261"/>
         <source>DLS Views (*.dlsv)</source>
         <translation>DLS-Ansichten (*.dlsv)</translation>
     </message>
     <message>
-        <location filename="MainWindow.cpp" line="251"/>
+        <location filename="MainWindow.cpp" line="262"/>
         <source>XML files (*.xml)</source>
         <translation>XML-Dateien (*.xml)</translation>
     </message>
     <message>
-        <location filename="MainWindow.cpp" line="252"/>
+        <location filename="MainWindow.cpp" line="263"/>
         <source>All files (*.*)</source>
         <translation>Alle Dateien (*.*)</translation>
     </message>
     <message>
-        <location filename="MainWindow.cpp" line="390"/>
+        <location filename="MainWindow.cpp" line="417"/>
         <source>Open data directory</source>
         <translation>Öffne Datenverzeichnis</translation>
     </message>
     <message>
-        <location filename="MainWindow.cpp" line="541"/>
+        <location filename="MainWindow.cpp" line="568"/>
         <source>Failed to start process.</source>
         <translation>Starten des Scriptes fehlgeschlagen.</translation>
     </message>
     <message>
-        <location filename="MainWindow.cpp" line="544"/>
+        <location filename="MainWindow.cpp" line="571"/>
         <source>Script crashed.</source>
         <translation>Script abgebrochen.</translation>
     </message>
     <message>
-        <location filename="MainWindow.cpp" line="547"/>
+        <location filename="MainWindow.cpp" line="574"/>
         <source>Script timed out.</source>
         <translation>Zeit für Script abgelaufen.</translation>
     </message>
     <message>
-        <location filename="MainWindow.cpp" line="550"/>
+        <location filename="MainWindow.cpp" line="577"/>
         <source>Failed to write to script.</source>
         <translation>Schreiben zum Script fehlgeschlagen.</translation>
     </message>
     <message>
-        <location filename="MainWindow.cpp" line="553"/>
+        <location filename="MainWindow.cpp" line="580"/>
         <source>Failed to read from script.</source>
         <translation>Lesen vom Script fehlgeschlagen.</translation>
     </message>
     <message>
-        <location filename="MainWindow.cpp" line="556"/>
+        <location filename="MainWindow.cpp" line="583"/>
         <source>Unknown error.</source>
         <translation>Unbekannter Fehler.</translation>
     </message>
     <message>
-        <location filename="MainWindow.cpp" line="560"/>
+        <location filename="MainWindow.cpp" line="587"/>
         <source>Script</source>
         <translation>Script</translation>
     </message>
     <message>
-        <location filename="MainWindow.cpp" line="561"/>
+        <location filename="MainWindow.cpp" line="588"/>
         <source>Failed to execute script: %1</source>
         <translation>Ausführen des Scriptes fehlgeschlagen: %1</translation>
     </message>
